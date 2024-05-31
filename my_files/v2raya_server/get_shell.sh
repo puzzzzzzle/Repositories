67c80a28a8f7f2bb0fc12ad4649470f05e06f0b9
puzzzzzzle <2359173906@qq.com>
2024-05-31 14:41:32 +0800

    update

--- my_files/v2raya_server/get_shell.sh
+++
@@ -1,2 +0,0 @@
-# v2ray server install from https://github.com/mack-a/v2ray-agent
-wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
